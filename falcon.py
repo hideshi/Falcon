6a92e109ea0d4642a283e378792aee58d458c715
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2015-03-13 08:31:08 +0800

    Added IndexManager, Tokenizer and argument parser

--- falcon.py
+++ falcon.py
@@ -1,25 +1,75 @@
 import sqlite3
 import pickle
 import re
+import argparse
 from collections import OrderedDict
 import pprint as pp
 
 def log(method):
     def wrapper(self, *args):
-        print('begin:', self.__class__.__name__, method.__name__)
-        print(args)
-        res = method(self, *args)
-        print('end:', self.__class__.__name__, method.__name__)
-        return res
+        if IndexManager.debug:
+            print('begin:', self.__class__.__name__, method.__name__)
+            print(args)
+        result = method(self, *args)
+        if IndexManager.debug:
+            print('end:', self.__class__.__name__, method.__name__)
+        return result
     return wrapper
 
-class Indexer(object):
+class Tokenizer(object):
+    @log
+    def __init__(self):
+        self.stopwords = re.compile(r'[\s,.!\?"\'\$%&#:;\{\}\[\]\(\)、。]')
 
     @log
-    def __init__(self, database_file):
+    def tokenize(self, word):
+        raise NotImplementedError("tokenize method must be overridden and implemented by a descendant class")
+
+    @log
+    def tokenize(self, title, content):
+        raise NotImplementedError("tokenize method must be overridden and implemented by a descendant class")
+
+class BigramTokenizer(Tokenizer):
+    @log
+    def tokenize(self, title, content = None):
+        if content != None:
+            document = title + content
+        length = len(document)
+        tokens = []
+        for i in range(0, length):
+            token = document[i:i+2]
+            m = self.stopwords.search(token)
+            if len(token) == 2 and m == None:
+                tokens.append((i, token))
+        return tokens
+
+class TrigramTokenizer(Tokenizer):
+    @log
+    def tokenize(self, title, content = None):
+        if content != None:
+            document = title + content
+        length = len(document)
+        tokens = []
+        for i in range(0, length):
+            token = document[i:i+3]
+            m = self.stopwords.search(token)
+            if len(token) == 3 and m == None:
+                tokens.append((i, token))
+        return tokens
+
+class TokenizerFactory(object):
+    def create_tokenizer(self, tokenizer):
+        if tokenizer == 'Bigram':
+            return BigramTokenizer()
+        elif tokenizer == 'Trigram':
+            return TrigramTokenizer()
+
+class Indexer(object):
+    @log
+    def __init__(self, database_file, tokenizer_type = 'Bigram'):
         self._database_file = database_file
+        self._tokenizer = TokenizerFactory().create_tokenizer(tokenizer_type)
         self._inverted_index = OrderedDict()
-        self.stopwords = re.compile(r'[\s,.]')
         connection = sqlite3.connect(self._database_file)
         with connection:
             cursor = connection.cursor()
@@ -38,7 +88,7 @@ class Indexer(object):
             """)
 
     @log
-    def create_new_index(self):
+    def delete_index(self):
         connection = sqlite3.connect(self._database_file)
         with connection:
             cursor = connection.cursor()
@@ -50,7 +100,7 @@ class Indexer(object):
         with connection:
             cursor = connection.cursor()
             cursor.execute('DELETE FROM documents')
-        #SELECT FROM documents
+        self.delete_index()
 
     @log
     def add_index(self, title, content, document_id = 0):
@@ -70,28 +120,23 @@ class Indexer(object):
 
     @log
     def _create_posting_list(self, document_id, title, content):
-        document = title + content
-        length = len(document)
-        for i in range(0, length):
-            token = document[i:i+2]
-            m = self.stopwords.search(token)
-            if len(token) == 2 and m == None:
-                if token in self._inverted_index:
-                    inverted_index_hash = self._inverted_index[token]
-                    inverted_index_hash.add(document_id, i)
-                else:
-                    connection = sqlite3.connect(self._database_file)
-                    with connection:
-                        cursor = connection.cursor()
-                        cursor.execute('SELECT token, posting_list FROM indexes WHERE token = ?', (token,))
-                        rows = cursor.fetchall()
-                        if len(rows) > 0:
-                            for row in rows:
-                                unpickled = pickle.loads(row[1])
-                                unpickled.add(document_id, i)
-                                self._inverted_index[token] = unpickled
-                        else:
-                            self._inverted_index[token] = InvertedIndexHash(token, document_id, i)
+        for i, token in self._tokenizer.tokenize(title, content):
+            if token in self._inverted_index:
+                inverted_index_hash = self._inverted_index[token]
+                inverted_index_hash.add(document_id, i)
+            else:
+                connection = sqlite3.connect(self._database_file)
+                with connection:
+                    cursor = connection.cursor()
+                    cursor.execute('SELECT token, posting_list FROM indexes WHERE token = ?', (token,))
+                    rows = cursor.fetchall()
+                    if len(rows) > 0:
+                        for row in rows:
+                            unpickled = pickle.loads(row[1])
+                            unpickled.add(document_id, i)
+                            self._inverted_index[token] = unpickled
+                    else:
+                        self._inverted_index[token] = InvertedIndexHash(token, document_id, i)
 
     @log
     def _flush_buffer(self):
@@ -103,7 +148,6 @@ class Indexer(object):
                 cursor.execute('INSERT OR REPLACE INTO indexes (token, posting_list) VALUES (?, ?)', (k, pickled))
 
 class InvertedIndexHash(object):
-
     @log
     def __init__(self, token, document_id, position):
         self.token = token
@@ -122,20 +166,55 @@ class InvertedIndexHash(object):
             self.posting_list[document_id] = [position]
             self.positions_count = self.positions_count + 1
 
-if __name__ == '__main__':
-    indexer = Indexer('test.db')
-    #indexer.create_new_index()
-    #indexer.delete_documents()
-    indexer.add_index('greeting', 'Good morning everyone.')
-    indexer.add_index('introduction', 'My name is Taro.')
+class Searcher(object):
+    @log
+    def search(self, word):
+        pass
+
+class IndexManager(object):
     
-    connection = sqlite3.connect('test.db')
-    with connection:
-        cursor = connection.cursor()
-        cursor.execute('SELECT id, title, content FROM documents')
-        for row in cursor.fetchall():
-            print(row[0], row[1], row[2])
-        cursor.execute('SELECT token, posting_list FROM indexes ORDER BY token')
-        for k1, v1 in cursor.fetchall():
-            o = pickle.loads(v1)
-            print(o.token, o.positions_count, o.posting_list)
+    debug = False
+
+    def run(self):
+        parser = argparse.ArgumentParser(description='Falcon Full Text Search Engine')
+        parser.add_argument('-D', '--debug', help='enable debug mode', action='store_true')
+        parser.add_argument('-I', '--showindex', help='show index', action='store_true')
+        parser.add_argument('-C', '--showdocument', help='show document(s)', action='store_true')
+        parser.add_argument('-c', '--content', metavar='content', help='document content to be stored and indexed')
+        parser.add_argument('-d', '--databasefile', metavar='databasefile', help='a sqlite3 database file')
+        parser.add_argument('-q', '--query', metavar='query', help='query string')
+        parser.add_argument('-t', '--title', metavar='title', help='document title to be stored and indexed')
+        parser.add_argument('-z', '--tokenizer', metavar='tokenizer', help='Type of tokenizer [Bigram, Trigram]')
+        parser.add_argument('files', metavar='files', nargs='*', help='input file(s)')
+        self._args = parser.parse_args()
+
+        IndexManager.debug = self._args.debug
+
+        if self._args.databasefile != None:
+            if self._args.query != None:
+                # Search
+                pass
+            elif self._args.title != None and self._args.content != None:
+                indexer = Indexer(self._args.databasefile, self._args.tokenizer)
+                indexer.add_index(self._args.title, self._args.content)
+
+            if self._args.showindex:
+                connection = sqlite3.connect(self._args.databasefile)
+                with connection:
+                    cursor = connection.cursor()
+                    cursor.execute('SELECT token, posting_list FROM indexes ORDER BY token')
+                    for k1, v1 in cursor.fetchall():
+                        o = pickle.loads(v1)
+                        print(o.token, o.positions_count, o.posting_list)
+
+            if self._args.showdocument:
+                connection = sqlite3.connect(self._args.databasefile)
+                with connection:
+                    cursor = connection.cursor()
+                    cursor.execute('SELECT id, title, content FROM documents')
+                    for row in cursor.fetchall():
+                        print(row[0], row[1], row[2])
+
+if __name__ == '__main__':
+    index_manager = IndexManager()
+    index_manager.run()
